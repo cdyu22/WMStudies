2ea4941d29f2aa67a637c8c71b1cf56649482a9b
Connor Yu <connordyu@gmail.com>
2021-01-05 19:27:33 -0500

    Reorganized the clutter. Going to make class models

--- MODULE_scraping/models.py
+++ MODULE_scraping/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model): #The class that will stand as a class.
+    # CRN
+    # Level + Section + Course name #SUBJECT OPTION IS REDUNDENT
+    # Status
+    # Amount of Followers? Could this create the subset of followed classes? Skip if 0?
+    pass
+
+class StudentTracker(models.Model): #The model hat will be one student tracking that class.
+    # CRN (How the scraper identifies the student)
+    # Student Username (How the student (client-side) identifies their CRNS)
+    pass
