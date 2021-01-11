d8e3519953d15f7a52e75afdddc6838a5649f5a1
Connor Yu <connordyu@gmail.com>
2021-01-11 14:08:54 -0500

    Deleted testing file.

--- MODULE_scraping/__init__.py
+++ MODULE_scraping/__init__.py
@@ -1,5 +1,5 @@
 from .WM_Subject import Subject_Scraper
-from secrets import link, token, user_key
+from ..secrets.secrets import link, token, user_key
 print("LOADED MODULE_SCRAPING")
 
 classes = Subject_Scraper(  2, link, token, user_key )
